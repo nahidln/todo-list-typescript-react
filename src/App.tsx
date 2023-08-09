edee356dcedaf662ea487d44cfa3ca5d8a18d681
nahidln <121415739+nahidln@users.noreply.github.com>
2023-08-09 09:15:37 +0530

    to do list project added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState<string>('');
+  const [tasklist, SetTaskList] = useState<string[]>([]);
+
+  const handleTask=(e:ChangeEvent<HTMLInputElement>)=>{
+    setTask(e.target.value)
+  }
+  const handleAddButton=()=>{
+    SetTaskList([...tasklist,task]);
+    setTask('')
+  }
+
+  const handleDelete=(e:React.MouseEvent<HTMLSpanElement>)=>{
+    const newList = tasklist.filter(item =>item !=e.currentTarget.id);
+    SetTaskList(newList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List Application</h2>
+      <div>
+        <input type='text' value={task} onChange={handleTask} />
+        <button className='btnAdd' onClick={handleAddButton}>Add</button>
+      </div>
+      <ul>
+        {
+          tasklist.map((item,index)=>
+          <li className='liItem' key={index}>
+            <span>{item}</span>
+            <span className='btnRemove' onClick={handleDelete} id={item}>X</span>
+          </li>
+          )
+        }
+      </ul>
     </div>
   );
 }
